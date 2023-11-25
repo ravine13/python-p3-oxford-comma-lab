5660e2cc325b5a8e8d850729fd5f67cbf290ebaa
ravine13 <kuriaderek@gmail.com>
2023-11-25 19:17:45 +0300

    passed test

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    if not str:
+        return ""
+    elif len(arr)==1:
+        return arr[0]
+    elif len(arr)==2:
+        return " and ".join(arr)
+    else:
+        return ", ".join(arr[:-1])+ f", and {arr[-1]}"
